dd6a15cd9a4ef23abe35cecbd7a8948df5901ddb
sivaprakash55 <47659052+sivaprakash55@users.noreply.github.com>
2024-12-06 20:17:22 +0530

    Add files via upload

---
+++ Print_Function.py
@@ -0,0 +1,8 @@
+print("How old are you ?", end=' ')
+age = int(input())
+print("How tall are you ?",end ='')
+height = int(input())
+print("How much weight are you ?",end='')
+weight = float(input())
+
+print(f"So, you're {age} old, {height} inches tall and {weight} kg's heavey.")
---
+++ Variables.py
@@ -0,0 +1,13 @@
+my_name = 'Siva Prakash'
+my_age = 30
+my_hieght = 74
+my_weigth = 80
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_hieght} inches tall")
+print(f"He's {my_weigth} pounds heavy")
+total = my_age + my_hieght + my_weigth
+print(f"If I add {my_age}, {my_hieght} and {my_weigth}. I get my {total} ")
